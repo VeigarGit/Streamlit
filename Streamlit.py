497a788592ce019390ed80542d1e6bbec58de901
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 15:05:33 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -4,11 +4,7 @@ import numpy as np
 
 df1 = st.file_uploader("Escolha um arquivo:", type="xlsx")
 
-print (df1)
+if df1:
+  st.dataframe(df1)
 
 
-
-df1.head()
-df1list = ['Coluna1','Coluna2']
-df1['results'] = df1[list].sum(axis=1)
-print(df1)
