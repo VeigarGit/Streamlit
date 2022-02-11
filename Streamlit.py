027568734eefdd3dc157b3030a35995ec6c17130
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 15:10:39 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 
-df1 = st.file_uploader("Escolha um arquivo:", type="xlsx")
-df = pd.read_excel(df1)
+lista_soma = list()
+arquivo = st.file_uploader("Escolha um arquivo:", type="xlsx")
 
-if df1:
-  st.dataframe(df)
+if arquivo:
+    df = pd.read_excel(arquivo)
+    st.dataframe(df)
 
+    for i in range(0, len(df)):
+        lista_soma.append(df['COLUNA 1'][i]+df['COLUNA 2'][i])
 
+    df_soma = pd.DataFrame(lista_soma, columns = ["SOMA"])
+    st.table(df_soma)
