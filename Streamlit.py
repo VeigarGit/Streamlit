1ea77c188a82ad0490d00ad6dba9286ae66ed4ea
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 14:27:36 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,8 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-xls = st.file_uploader("Escolha um arquivo:", type="xlsx")
+xls = pd.ExcelFile(r'C:\Users\Rafael\Desktop\python\Streamlit\Streamlit\Streamlit\Somatoria.xlsx')
+df1 = pd.read_excel(xls)
+print (df1)
 
-xlslist= ['Coluna1','Coluna2']
-xls['results'] = xlslist[list].sum(axis=1)
-print(xls)
+
+df1.head()
+df1list = ['Coluna1','Coluna2']
+df1['results'] = df1[list].sum(axis=1)
+print(df1)
