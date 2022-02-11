0c1673052fcffde367bc0387360a27efbe2d4450
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 13:08:57 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -4,5 +4,6 @@ import pandas as pd
 st.write("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6]})
+df['result'] = 
 df_result = df.groupby(["one","two"]).sum(axis=1)
 st.write(df)
