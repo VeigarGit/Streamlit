48edf8e1cc4eacdc3e1b111b5e1c29c61bc5c828
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 12:45:22 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,15 +1,10 @@
 import streamlit as st
 import pandas as pd
-import connect from gsheetsdb 
+from gsheetsdb import connect 
 
 st.title("My First Streamlit Web App")
 
 df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
-st.title("Connect to Google Sheets")
-gsheet_url = "https://docs.google.com/spreadsheets/d/11-inHHG35pbGF3WyDZPBMPRcXVQoEGfkFt5RhxbBLQM/edit?usp=sharing"
-conn = connect()
-rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
-df_gsheet = pd.DataFrame(rows)
-st.write(df_gsheet)
+
