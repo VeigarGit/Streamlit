b8f2fdf1d28ab17ee6ccc280d5d83be4bc4e547e
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 09:18:01 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import pandas as pd
-st.title("execute app")
+import numpy as np
 
-df = pd.read_excel("Somatoria.xlsx")
-st.table(df)
+xls = pd.ExcelFile(r'C:\Users\Rafael\Desktop\python\Streamlit\Streamlit\Streamlit\Somatoria.xlsx')
+df1 = pd.read_excel(xls, 'Plan1')
+df2 = pd.DataFrame.to_excel(xls, 'Plan2')
+print (df1)
+print (df2)
+
+df1.head()
+df1list = ['Coluna1','Coluna2']
+df1['results'] = df1[list].sum(axis=1)
+print(df1)
