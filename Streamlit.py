76922f5c11c832f951de300b6f9e6ee73b9754e8
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 12:43:47 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -8,7 +8,7 @@ df = pd.DataFrame({"one": [1, 2, 3], "two": [4, 5, 6], "three": [7, 8, 9]})
 st.write(df)
 
 st.title("Connect to Google Sheets")
-gsheet_url = "https://docs.google.com/spreadsheets/d/1ixMrhGV1TPn14_oTyEIFjszuwuwO9xkbsc1WEBJH3N0/edit?usp=sharing"
+gsheet_url = "https://docs.google.com/spreadsheets/d/11-inHHG35pbGF3WyDZPBMPRcXVQoEGfkFt5RhxbBLQM/edit?usp=sharing"
 conn = connect()
 rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
 df_gsheet = pd.DataFrame(rows)
