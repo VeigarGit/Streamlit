4800cffe3d38aa0d0febbbc0efdc5f1aa996c729
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 15:07:13 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,7 +3,8 @@ import pandas as pd
 import numpy as np
 
 df1 = st.file_uploader("Escolha um arquivo:", type="xlsx")
-df = pd.DataFrame(df1)
+df = pd.read_exceldf1)
+
 if df1:
   st.dataframe(df1)
 
