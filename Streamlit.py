fea4320d013a09ef2c8e0bbf54d220c638eb8519
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 14:25:28 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -3,11 +3,7 @@ import pandas as pd
 import numpy as np
 
 xls = st.file_uploader("Escolha um arquivo:", type="xlsx")
-df1 = pd.read_excel(xls)
-print (df1)
 
-
-df1.head()
-df1list = ['Coluna1','Coluna2']
-df1['results'] = df1[list].sum(axis=1)
-print(df1)
+xlslist= ['Coluna1','Coluna2']
+xls['results'] = xlslist[list].sum(axis=1)
+print(xls)
