7c2955e74ecd9f740bd0eac2d48ac813eeb5ac7c
Rafael Veiga <55032777+VeigarGit@users.noreply.github.com>
2022-02-11 12:37:39 -0300

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np 
 
-public_gsheets_url = "https://docs.google.com/spreadsheets/d/xxxxxxx/edit#gid=0"
-
+x = public_gsheets_url = "https://docs.google.com/spreadsheets/d/xxxxxxx/edit#gid=0"
+print(x)
